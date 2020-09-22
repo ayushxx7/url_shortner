21b359eb91878cab8d3ea6d427a2bc0913c2b04d
Ayush <ayushxx7@gmail.com>
2020-09-22 21:17:25 +0530

    feat(token): token issue logic added. added model for url shortner

--- url_sh/views.py
+++ url_sh/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import short_urls
+from .forms import UrlForm
+from .shortner import shortner
 
 # Create your views here.
+def Make(request):
+    form = UrlForm(request.POST)
+    token = ""
+    if request.method == "POST":
+        if form.is_valid():
+            NewUrl = form.save(commit=False)
+            token = shortner().issue_token()
+            NewUrl.save()
+        else:
+            form = UrlForm()
+            token = "Invalid URL"
+    return render(request, 'home.html', {'form': form, 'token': token})
--- url_shortner/urls.py
+++ url_shortner/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from url_sh import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.Make, name="Make New")
 ]
